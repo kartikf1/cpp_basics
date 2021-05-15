91f117a259c58e90061830d135f354c33f37b7c0
yogesh kartik <kartikyogesh2004@gmail.com>
2021-05-15 23:37:48 +0530

    work is pending...

---
+++ input/testtwo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    cout << "Just testing that all setup is working right or not.";
+    // No the output of this file is not comming in output folder.
+    // I have to search on this.
+    // But today its already 23:35 pm, so i am going to sleep.
+    // Good night ~(*_*)~
+    return 0;
+}
