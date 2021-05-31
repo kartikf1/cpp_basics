09fa98a16cbad3bab95eb9bbe6a5d2c8bfed6176
yogesh kartik <kartikyogesh2004@gmail.com>
2021-05-31 17:17:15 +0530

    playing with pointers

--- input/pointer2.cpp
+++ input/pointer2.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 main()
 {
-    string str = {"apple"};
+    string str = {};
     int i = 0;
+    string wordfinal;
     for (string word; i < 3; i++)
     {
         cout << "Please enter any word " << i + 1 << ": ";
-        cin.ignore(); // I have to use it to clear apce of previous cout.
+        //cin.ignore(); // I have to use it to clear apce of previous cout.
         getline(cin, word);
-        cout << word;
+        //cout << word;
         //cin >> word; // cin only take char as a input, means only 1 character.
         // So i have to use getline() function
-        //str.append(word+" ");
-        //ystr[i] = word;
+        wordfinal = "\"" + word + "\", ";
+        //str.append(wordfinal);
+        str[i] = wordfinal;
         cout << str << "\n";
     }
     cout << "Now printing array backward.\n";
