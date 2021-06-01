cfd1efdea13d8649c914f11e44f9d3a2ed47c78a
yogesh kartik <kartikyogesh2004@gmail.com>
2021-06-01 12:26:11 +0530

    added comment

--- input/function4.cpp
+++ input/function4.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-void myfun(int num1, int num2)
+void myfun(int num1, int num2)  // This num1 and num2 are called parameters.
 {
     cout << "The sum of " << num1 << " and " << num2 << " is: " << num1 + num2 << "\n";
     cout << "The substraction of " << num1 << " and " << num2 << " is: " << num1 - num2 << "\n";
@@ -18,6 +18,6 @@ main()
     cin >> a;
     cout << str2;
     cin >> b;
-    myfun(a, b);
+    myfun(a, b);    // This a and b are called arguments.
     return 0;
 }
