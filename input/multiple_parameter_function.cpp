60a449d0f6867d61932096a9fd5dd2b703b7d39b
yogesh kartik <kartikyogesh2004@gmail.com>
2021-06-02 22:58:48 +0530

    debuging it.

---
+++ input/multiple_parameter_function.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string str1;
+
+//void fun();
+
+void fun1(string word = str1)
+{
+    for (char letter, posnum; int a = 0 < sizeof(word) + 1; a++)
+    {
+        letter = word[a];
+        posnum = a;
+        //fun();
+        cout << "The position of \t"<< letter << "is \t"<< posnum << "\n";
+    };
+}
+
+main()
+{
+    //string word;
+    cout << "Please enter any word : ";
+    getline(cin, str1);
+    //word = str1;
+    cout << "\n";
+    fun1();
+    return 0;
+}
+/*
+void fun(char j = letter, int k =  posnum)
+{
+    cout << j << "Position number : " << k;
+}*/
